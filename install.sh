a1edfb0c1a2d2c451e1603c9c4568e9434250842
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2017-05-01 09:58:22 +0100

    :bug: Add link creation for .vimrc
    Closes #5

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ rsync --exclude ".git/" \
 	--exclude "README.md" \
 	-avh --no-perms . ~;
 
+
+# This line needs to be commented if you're not using the
+# default vim config
+ln -s ~/.vim/.vimrc ~/.vimrc
