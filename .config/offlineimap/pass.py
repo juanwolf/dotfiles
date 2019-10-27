b1ef5b2b6f192ffa061ef800fd04ca4aaf951aba
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-10-27 13:41:10 +0000

    :sparkles:(offlineimap): Add gmail support

--- .config/offlineimap/pass.py
+++ .config/offlineimap/pass.py
@@ -4,6 +4,7 @@ from subprocess import check_output
 
 EMAIL_PASS_ROOT="Email/"
 LOGIN_PREFIX="login: "
+REFRESH_TOKEN_PREFIX="RefreshToken: "
 
 
 def get_password(account):
@@ -16,6 +17,15 @@ def get_username(account):
         if line.startswith(LOGIN_PREFIX):
             login = line[len(LOGIN_PREFIX):len(line)]
             return login
+    return ""
+
+
+def get_refresh_token(account):
+    for line in  check_output("pass " + EMAIL_PASS_ROOT + account, shell=True).splitlines():
+        line = line.decode("utf-8")
+        if line.startswith(REFRESH_TOKEN_PREFIX):
+            refresh_token = line[len(REFRESH_TOKEN_PREFIX):len(line)]
+            return refresh_token.encode('utf-8')
     return ""
 
 
