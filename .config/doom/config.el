5b925ebca48034e117a0ae3974f42e6570cff435
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2021-01-04 11:35:15 +0000

    :sparkles:(doom): Use declarative config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,55 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Jean-Loup Adde"
-      user-mail-address "jean-loup.adde@mailfence.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "MesloLGL Nerd Font" :size 12 :weight 'normal)
-       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-city-lights)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/projects/42")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-(setq shr-use-fonts nil)
-
-(after! org
-  (setq org-adapt-indentation t
-        org-startup-indented nil
-        org-hide-leading-stars t
-        org-odd-levels-only t))
-
-(after! elfeed
-  (require 'elfeed-org)
-  (elfeed-org)
-  (setq rmh-elfeed-org-files (list "~/projects/42/feeds.org"))
-
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
@@ -66,3 +16,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+
+(when (file-exists-p "~/.config/doom/slayer.org")
+  (org-babel-load-file "~/.config/doom/slayer.org"))
+
+(when (file-exists-p "~/.emacs-private.org")
+  (org-babel-load-file "~/.emacs-private.org"))
